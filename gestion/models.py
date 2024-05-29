5800ca692b1de55922562d3c376c7adafb36f6f1
wilsonjosue <wturpoh@unsa.edu.pe>
2024-05-29 10:52:42 -0500

    Se define el modelo alumno en la direcciongestion/models.py

--- gestion/models.py
+++ gestion/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=200)
+    apellido = models.CharField(max_length=200)
+    matricula = models.CharField(max_length=200, unique=True)
+
+
+
