7d2632507e96019970e13d98501625a9d290b1f9
wilsonjosue <wturpoh@unsa.edu.pe>
2024-05-29 11:51:37 -0500

    Se crea el metodo crearAlumno

--- gestion/views.py
+++ gestion/views.py
@@ -4,4 +4,13 @@ from .forms import AlumnoForm, CursoForm, NotaForm
 from .models import Alumno, Curso, Nota
 # Create your views here.
 
-def crearAlumno():
+def crearAlumno(request):
+    if request.method == 'POST':
+        form = AlumnoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listaAlumnos')
+    else:
+        form = AlumnoForm()
+    return render(request, 'gestion/crearAlumno.html', {'form': form}) 
+ 
