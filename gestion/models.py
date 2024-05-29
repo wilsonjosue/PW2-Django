2cd8f555a533bfeed0142350a8aae071b6e82ea3
wilsonjosue <wturpoh@unsa.edu.pe>
2024-05-29 11:03:55 -0500

    Se agrega la clase Curso, se agrega la funcion str mostrar el nombre del curso y se modifica  clase Alumno  en la direccion gestion/models.py

--- gestion/models.py
+++ gestion/models.py
@@ -5,11 +5,17 @@ from django.db import models
 class Alumno(models.Model):
     nombre = models.CharField(max_length=200)
     apellido = models.CharField(max_length=200)
-    matricula = models.CharField(max_length=200, unique=True)
+    matricula = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return f"{self.nombre} {self.apellido}."
     
-class 
+class Curso (models.Model):
+    nombre = models.CharField(max_length=200)
+    codigo = models.CharField(max_length=100, unique=True)
 
+    def __str__(self):
+        return self.nombre
+
+   
 
