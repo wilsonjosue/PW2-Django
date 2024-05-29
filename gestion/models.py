60206d79857bc3eaa86db425f91421a33359d5a9
wilsonjosue <wturpoh@unsa.edu.pe>
2024-05-29 11:13:46 -0500

    Se arregla un poco el codigo

--- gestion/models.py
+++ gestion/models.py
@@ -10,12 +10,14 @@ class Alumno(models.Model):
     def __str__(self):
         return f"{self.nombre} {self.apellido}."
     
-class Curso (models.Model):
+class Curso(models.Model):
     nombre = models.CharField(max_length=200)
     codigo = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.nombre
+
+class Nota 
 
    
 
