eeb62fac3859a1ab4d8e3f21cf1dc89e16ce4438
wilsonjosue <wturpoh@unsa.edu.pe>
2024-05-29 11:44:17 -0500

    Se importan forms y model  y se empieza a definir las vistas

--- gestion/views.py
+++ gestion/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import AlumnoForm, CursoForm, NotaForm
+from .models import Alumno, Curso, Nota
 # Create your views here.
+
+def crearAlumno():
