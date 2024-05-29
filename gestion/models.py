152489661ed2bf908a5b00e172644c3eecfd5fac
wilsonjosue <wturpoh@unsa.edu.pe>
2024-05-29 11:19:31 -0500

    Se define el modelo Nota y se agrega los atributos que manejara con su configuracion  en la direccion gestion/models.py

--- gestion/models.py
+++ gestion/models.py
@@ -17,7 +17,10 @@ class Curso(models.Model):
     def __str__(self):
         return self.nombre
 
-class Nota 
+class Nota(models.Model): 
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    nota = models.DecimalField(max_digits=5, decimal_places=2)
 
    
 
